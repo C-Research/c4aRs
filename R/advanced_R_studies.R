6a4938f7dff146496edf56f07cccaaf09bfad253
mmkearns16 <mmkearns16@gmail.com>
2020-02-10 12:58:18 -0500

    added color palettes

--- R/advanced_R_studies.R
+++ R/advanced_R_studies.R
@@ -8,10 +8,6 @@
 #' data(world_map)
 #'
 
-world_map <- read.csv("data/world_map.csv", stringsAsFactors = F, sep = ",", colClasses = "character")
-world_map$long <- as.numeric(world_map$long)
-world_map$lat <- as.numeric(world_map$lat)
-
 
 
 
@@ -60,10 +56,18 @@ fixCountryCol <- function(inVec){
 }
 
 
-leafletLabel <- function(df, textNames, colNames, titleCol = NULL, title = NULL, size = 2, titleSize = 4){
+leafletLabel <- function(df, textNames, colNames, titleCol = NULL, title = NULL, size = 2, titleSize = 4, digits = 2){
   if (length(textNames) != length(colNames)){
     print("The number of names doesn't match the number of columns!")
     return("The number of names doesn't match the number of columns!")
+  }
+
+  roundFunc <- function(lab, digs = digits){
+    if (is.numeric(lab)){
+      return(round(lab, digs))
+    }else{
+      return(lab)
+    }
   }
 
   num <- length(textNames)
@@ -85,13 +89,13 @@ leafletLabel <- function(df, textNames, colNames, titleCol = NULL, title = NULL,
     for (i in 1:(num - 1)){
       df <-
         df %>%
-        mutate(label = paste0(label, "<b><font size=", size, ">", textNames[i], "</font></b>: ", eval(as.name(colNames[i])), "</br>"))
+        mutate(label = paste0(label, "<b><font size=", size, ">", roundFunc(textNames[i]), "</font></b>: ", eval(as.name(colNames[i])), "</br>"))
     }
   }
 
   df <-
     df %>%
-    mutate(label = paste0(label, "<b><font size=", size, ">", textNames[num], "</font></b>: ", eval(as.name(colNames[num]))))
+    mutate(label = paste0(label, "<b><font size=", size, ">", roundFunc(textNames[num]), "</font></b>: ", eval(as.name(colNames[num]))))
 
   return(df)
 }
@@ -135,8 +139,8 @@ plotlyLabel <- function(df, textNames, colNames, titleCol = NULL, title = NULL){
 
 fixMultCols <- function(df, cols, old, new){
   df %>%
-    mutate_at(cols, ~str_replace(., old, new)) %>%
-    mutate_at(cols, trimws) %>%
+    dplyr::mutate_at(cols, ~str_replace(., old, new)) %>%
+    dplyr::mutate_at(cols, trimws) %>%
     return()
 }
 selectByMultCols <- function(df, cols, values){
@@ -211,5 +215,55 @@ traffickingInstanceCount <- function(df, cols, by_col = NA){
 
 
 
+
+scale_fill_ROUTES_modeofT <- function(...){
+  ggplot2:::manual_scale(
+    'fill',
+    values = setNames(brewer.pal(6, 'Accent'),
+                      c("Air Freight","Checked Luggage","Mail",
+                        "Passenger\nClothing/Items","Unknown", "Private Plane")),
+    ...
+  )
+}
+scale_fill_ROUTES_dbs <- function(...){
+  ggplot2:::manual_scale(
+    'fill',
+    values = setNames(c("#FF8C00", "#FF0000", "#008B00", "#4F94CD", "#DB7093", "#126B62", "#DAA520"),
+                      c("Ivory", "Rhino Horn", "Reptiles", "Birds", "Pangolin", "Marine Species", "Mammals")),
+    ...
+  )
+}
+
+
+
+
+c4_cols_blacks        <- c("#1d1d1b")
+c4_cols_greys         <- c("#414042", "#636362", '#9B9B9B', '#CFCFCF')
+c4_cols_reds          <- c("#f75151", '#EF8479', '#F5B1A5', '#F9DAD2')
+c4_cols_blues         <- c("#043D5D", '#485A77', '#81889F', '#BEBFCD')
+c4_cols_olives        <- c("#686B30", '#8E894E', '#B2AC83', '#D7D3BF')
+c4_cols_teals         <- c("#004342", '#3F6363', '#7B8E8E', '#BBC2C2')
+c4_cols_purples       <- c("#122945", '#484C64', '#7C7C8F', '#B9B7C2')
+c4_cols_yellows       <- c("#D4AD6D", '#E2C48F', '#EBD8B5', '#F5EADA')
+c4_cols_greens        <- c("#05A07E", '#6BB298', '#A3CBB9', '#D2E5DD')
+c4_cols_light_blues   <- c("#A0B9D0", '#C3CFE2', '#D6DEEA', '#EBEFF5')
+c4_cols_tans          <- c("#F8E5C8", '#FBEFDB', '#FCF4E7', '#FEF9F3')
+c4_cols_green_greys   <- c("#B3B299", '#BCB69B', '#DDDACD', '#EDECE6')
+
+
+c4_cols_all <- c(
+  "black" = "#1d1d1b",
+  "grey" = "#414042",
+  "red" = "#f75151",
+  "blue" = "#043D5D",
+  "olive" = "#686B30",
+  "teal" = "#004342",
+  "purple" = "#122945",
+  "yellow" = "#D4AD6D",
+  "green" = "#05A07E",
+  "light_blue" = "#A0B9D0",
+  "tan" = "#F8E5C8",
+  "green_grey" = "#B3B299"
+)
 
 
