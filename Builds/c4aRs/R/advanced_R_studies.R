05c2a384485e0789875e0568556517ef7947503a
mmkearns16 <mmkearns16@gmail.com>
2020-09-10 13:31:39 -0400

    cran compatibility updates

---
+++ Builds/c4aRs/R/advanced_R_studies.R
@@ -0,0 +1,23 @@
+#' @docType data
+#'
+#' @usage data(world_map)
+#'
+#' @return
+#'
+#' @examples For now, this package does nothing but export the corrected World Map.
+#' data(world_map)
+#'
+
+world_map <- read.csv("data/world_map.csv", stringsAsFactors = F, sep = ",")
+
+
+
+
+
+
+
+
+
+
+
+
--- R/advanced_R_studies.R
+++ R/advanced_R_studies.R
@@ -7,9 +7,10 @@
 #' @examples For now, this package does nothing but export the corrected World Map.
 #' data(world_map)
 #'
+#' @importFrom rlang .data
 
 
-
+`%>%` <-  magrittr::`%>%`
 
 
 fixCountryCol <- function(inVec){
@@ -57,6 +58,9 @@ fixCountryCol <- function(inVec){
 
 
 leafletLabel <- function(df, textNames, colNames, titleCol = NULL, title = NULL, size = 2, titleSize = 4, digits = 2){
+
+  label <- NULL # make sure does not look like an unbound reference
+
   if (length(textNames) != length(colNames)){
     print("The number of names doesn't match the number of columns!")
     return("The number of names doesn't match the number of columns!")
@@ -75,31 +79,34 @@ leafletLabel <- function(df, textNames, colNames, titleCol = NULL, title = NULL,
   if (!is.null(title)){
     df <-
       df %>%
-      mutate(label = paste0("<b><font size=", titleSize, ">", title, "</font></b></br>"))
+      dplyr::mutate(label = paste0("<b><font size=", titleSize, ">", title, "</font></b></br>"))
   }else if (!is.null(titleCol)){
     df <-
       df %>%
-      mutate(label = paste0("<b><font size=", titleSize, ">", eval(as.name(titleCol)), "</font></b></br>"))
+      dplyr::mutate(label = paste0("<b><font size=", titleSize, ">", eval(as.name(titleCol)), "</font></b></br>"))
   }else{
     df <-
       df %>%
-      mutate(label = "")
+      dplyr::mutate(label = "")
   }
   if (num != 1){
     for (i in 1:(num - 1)){
       df <-
         df %>%
-        mutate(label = paste0(label, "<b><font size=", size, ">", roundFunc(textNames[i]), "</font></b>: ", eval(as.name(colNames[i])), "</br>"))
+        dplyr::mutate(label = paste0(label, "<b><font size=", size, ">", roundFunc(textNames[i]), "</font></b>: ", eval(as.name(colNames[i])), "</br>"))
     }
   }
 
   df <-
     df %>%
-    mutate(label = paste0(label, "<b><font size=", size, ">", roundFunc(textNames[num]), "</font></b>: ", eval(as.name(colNames[num]))))
+    dplyr::mutate(label = paste0(label, "<b><font size=", size, ">", roundFunc(textNames[num]), "</font></b>: ", eval(as.name(colNames[num]))))
 
   return(df)
 }
 plotlyLabel <- function(df, textNames, colNames, titleCol = NULL, title = NULL){
+
+  label <- NULL # make sure does not look like an unbound reference
+
   if (length(textNames) != length(colNames)){
     print("The number of names doesn't match the number of columns!")
     return("The number of names doesn't match the number of columns!")
@@ -110,27 +117,27 @@ plotlyLabel <- function(df, textNames, colNames, titleCol = NULL, title = NULL){
   if (!is.null(title)){
     df <-
       df %>%
-      mutate(label = paste0("<b>", title, "</b></br>"))
+      dplyr::mutate(label = paste0("<b>", title, "</b></br>"))
   }else if (!is.null(titleCol)){
     df <-
       df %>%
-      mutate(label = paste0("<b>", eval(as.name(titleCol)), "</b></br>"))
+      dplyr::mutate(label = paste0("<b>", eval(as.name(titleCol)), "</b></br>"))
   }else{
     df <-
       df %>%
-      mutate(label = "")
+      dplyr::mutate(label = "")
   }
   if (num != 1){
     for (i in 1:(num - 1)){
       df <-
         df %>%
-        mutate(label = paste0(label, "<b>", textNames[i], "</b>: ", eval(as.name(colNames[i])), "</br>"))
+        dplyr::mutate(label = paste0(label, "<b>", textNames[i], "</b>: ", eval(as.name(colNames[i])), "</br>"))
     }
   }
 
   df <-
     df %>%
-    mutate(label = paste0(label, "<b>", textNames[num], "</b>: ", eval(as.name(colNames[num]))))
+    dplyr::mutate(label = paste0(label, "<b>", textNames[num], "</b>: ", eval(as.name(colNames[num]))))
 
   return(df)
 }
@@ -144,70 +151,85 @@ fixMultCols <- function(df, cols, old, new){
     return()
 }
 selectByMultCols <- function(df, cols, values){
+  `.` <- NULL # make sure does not look like an unbound reference
+
   df %>%
-    filter_at(cols, any_vars(. %in% values)) %>%
+    dplyr::filter_at(cols, dplyr::any_vars(. %in% values)) %>%
     return()
 }
 
 
 
 traffickingInstanceCount <- function(df, cols, by_col = NA){
+
+  count <- NULL # make sure does not look like an unbound reference
+  n <- NULL # make sure does not look like an unbound reference
+  total <- NULL # make sure does not look like an unbound reference
+  val <- NULL # make sure does not look like an unbound reference
+  `.` <- NULL # make sure does not look like an unbound reference
+
   df <-
     df %>%
-    mutate_if(is.factor, as.character)
+    dplyr::mutate_if(is.factor, as.character)
   if (is.na(by_col)){
       df %>%
       dplyr::select(cols) %>%
       split(seq(nrow(df))) %>%
-      map(unlist) %>%
-      map(~str_split(., "/")) %>%
-      map(unlist) %>%
-      map(trimws) %>%
-      map(unique) %>%
-      map(~.[!is.na(.)]) %>%
+      purrr::map(unlist) %>%
+      purrr::map(~str_split(., "/")) %>%
+      purrr::map(unlist) %>%
+      purrr::map(trimws) %>%
+      purrr::map(unique) %>%
+      purrr::map(~.[!is.na(.)]) %>%
       unlist() %>%
       table() %>%
       as.data.frame(stringsAdFactors = F) %>%
-      rename(count = Freq, value = ".") %>%
-      arrange(-count) %>%
+      dplyr::rename(count = "Freq", value = ".") %>%
+      dplyr::arrange(-count) %>%
       return()
   }else{
     helpFunc1 <- function(row, cols, by){
       list(
         values = row %>%
           dplyr::select(cols) %>%
-          map(unique) %>%
-          map(~.[!is.na(.)]) %>%
+          purrr::map(unique) %>%
+          purrr::map(~.[!is.na(.)]) %>%
           unlist() %>%
           unique() %>%
           as.matrix(ncol = 1) %>%
           as.data.frame(stringsAsFactors = F) %>%
-          rename(val = "V1") %>%
-          separate_rows(val, sep = "/") %>%
-          mutate(val = trimws(val)),
+          dplyr::rename(val = "V1") %>%
+          tidyr::separate_rows(val, sep = "/") %>%
+          dplyr::mutate(val = trimws(val)),
         ids = row %>%
           dplyr::select(by)
       ) %>%
         return()
     }
     helpFunc2 <- function(row){
-      cbind(row$values, row$ids, row.names = NULL)
+      if (nrow(row$values) > 0){
+        cbind(row$values, row$ids, row.names = NULL) %>%
+          return()
+      }else{
+        return(NULL)
+      }
+
     }
 
       df %>%
       dplyr::select(c(cols, by_col)) %>%
       split(seq(nrow(df))) %>%
-      map(helpFunc1, cols = cols, by = by_col) %>%
-      map(helpFunc2) %>%
-      bind_rows %>%
-      group_by_all() %>%
-      tally() %>%
-      arrange(-n) %>%
-      ungroup() %>%
-      pivot_wider(id_cols = val, names_from = by_col, values_from = n) %>%
-      mutate_if(is.numeric, replace_na, replace = 0) %>%
-      mutate(total = rowSums(.[-1])) %>%
-      arrange(-total) %>%
+      purrr::map(helpFunc1, cols = cols, by = by_col) %>%
+      purrr::map(helpFunc2) %>%
+      dplyr::bind_rows() %>%
+      dplyr::group_by_all() %>%
+      dplyr::tally() %>%
+      dplyr::arrange(-n) %>%
+      dplyr::ungroup() %>%
+      tidyr::pivot_wider(id_cols = val, names_from = by_col, values_from = n) %>%
+      dplyr::mutate_if(is.numeric, tidyr::replace_na, replace = 0) %>%
+      dplyr::mutate(total = rowSums(.[-1])) %>%
+      dplyr::arrange(-total) %>%
       return()
   }
 }
@@ -216,54 +238,37 @@ traffickingInstanceCount <- function(df, cols, by_col = NA){
 
 
 
-scale_fill_ROUTES_modeofT <- function(...){
-  ggplot2:::manual_scale(
-    'fill',
-    values = setNames(brewer.pal(6, 'Accent'),
-                      c("Air Freight","Checked Luggage","Mail",
-                        "Passenger\nClothing/Items","Unknown", "Private Plane")),
-    ...
-  )
-}
-scale_fill_ROUTES_dbs <- function(...){
-  ggplot2:::manual_scale(
-    'fill',
-    values = setNames(c("#FF8C00", "#FF0000", "#008B00", "#4F94CD", "#DB7093", "#126B62", "#DAA520"),
-                      c("Ivory", "Rhino Horn", "Reptiles", "Birds", "Pangolin", "Marine Species", "Mammals")),
-    ...
-  )
-}
 
-
-
-
-c4_cols_blacks        <- c("#1d1d1b")
-c4_cols_greys         <- c("#414042", "#636362", '#9B9B9B', '#CFCFCF')
-c4_cols_reds          <- c("#f75151", '#EF8479', '#F5B1A5', '#F9DAD2')
-c4_cols_blues         <- c("#043D5D", '#485A77', '#81889F', '#BEBFCD')
-c4_cols_olives        <- c("#686B30", '#8E894E', '#B2AC83', '#D7D3BF')
-c4_cols_teals         <- c("#004342", '#3F6363', '#7B8E8E', '#BBC2C2')
-c4_cols_purples       <- c("#122945", '#484C64', '#7C7C8F', '#B9B7C2')
-c4_cols_yellows       <- c("#D4AD6D", '#E2C48F', '#EBD8B5', '#F5EADA')
-c4_cols_greens        <- c("#05A07E", '#6BB298', '#A3CBB9', '#D2E5DD')
-c4_cols_light_blues   <- c("#A0B9D0", '#C3CFE2', '#D6DEEA', '#EBEFF5')
-c4_cols_tans          <- c("#F8E5C8", '#FBEFDB', '#FCF4E7', '#FEF9F3')
-c4_cols_green_greys   <- c("#B3B299", '#BCB69B', '#DDDACD', '#EDECE6')
-
-
-c4_cols_all <- c(
-  "black" = "#1d1d1b",
-  "grey" = "#414042",
-  "red" = "#f75151",
-  "blue" = "#043D5D",
-  "olive" = "#686B30",
-  "teal" = "#004342",
-  "purple" = "#122945",
-  "yellow" = "#D4AD6D",
-  "green" = "#05A07E",
-  "light_blue" = "#A0B9D0",
-  "tan" = "#F8E5C8",
-  "green_grey" = "#B3B299"
+c4_col_gradients <- list(
+  "greys" = c("#414042", "#636362", '#9B9B9B', '#CFCFCF'),
+  "reds" = c("#f75151", '#EF8479', '#F5B1A5', '#F9DAD2'),
+  "blues" = c("#043D5D", '#485A77', '#81889F', '#BEBFCD'),
+  "olives" = c("#686B30", '#8E894E', '#B2AC83', '#D7D3BF'),
+  "teals" = c("#004342", '#3F6363', '#7B8E8E', '#BBC2C2'),
+  "purples" = c("#122945", '#484C64', '#7C7C8F', '#B9B7C2'),
+  "yellows" = c("#D4AD6D", '#E2C48F', '#EBD8B5', '#F5EADA'),
+  "greens" = c("#05A07E", '#6BB298', '#A3CBB9', '#D2E5DD'),
+  "light_blues" = c("#A0B9D0", '#C3CFE2', '#D6DEEA', '#EBEFF5'),
+  "tans" = c("#F8E5C8", '#FBEFDB', '#FCF4E7', '#FEF9F3'),
+  "green_greys" = c("#B3B299", '#BCB69B', '#DDDACD', '#EDECE6')
 )
+
+c4_cols <- c(
+  "greys" = "#414042",
+  "reds" = "#f75151",
+  "blues" = "#043D5D",
+  "olives" = "#686B30",
+  "teals" = "#004342",
+  "purples" = "#122945",
+  "yellows" = "#D4AD6D",
+  "greens" = "#05A07E",
+  "light_blues" = "#A0B9D0",
+  "tans" = "#F8E5C8",
+  "green_greys" = "#B3B299"
+)
+
+c4_col_leafy <- c('#f75151', "#414042", '#B3B299', "#05A07E", '#004342')
+c4_col_spooky <- c('#f75151', '#414042','#A0B9D0','#122945', '#043D5D')
+c4_col_dusty <- c('#f75151', '#414042', '#F8E5C8', '#D4AD6D', '#686B30')
 
 
